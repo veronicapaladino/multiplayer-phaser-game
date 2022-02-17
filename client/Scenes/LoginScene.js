43e38e7e11389cdfb43d9ace535e81fd04fe09be
Veronica Paladino <veronica.paladino@mercadolibre.com>
2022-02-17 20:49:16 -0300

    se comenta login que no anda

--- client/Scenes/LoginScene.js
+++ client/Scenes/LoginScene.js
@@ -1,4 +1,29 @@
 //Escena inicial del juego...
+class LoginScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "LoginScene" });
+  }
+
+  create() {
+    //this.add.sprite(0, 100, 'mouse');
+
+    this.input.mouse.capture = true;
+
+    const empezarPartida = this.add.text(100, 100, "Este es el login!!", {
+      fill: "#0f0",
+    });
+    empezarPartida.setInteractive();
+
+    empezarPartida.on("pointerdown", () => {
+      console.log("cambiar de escena a: MenuScene");
+      this.scene.start("MenuScene");
+    });
+  }
+}
+
+export default LoginScene;
+
+/* ESTO NO FUNCIONA!!
 var game2;
 class LoginScene extends Phaser.Scene{
 
@@ -55,4 +80,4 @@ class LoginScene extends Phaser.Scene{
 }
 
 
-export default LoginScene;
+export default LoginScene; */
