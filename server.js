8e9b6dd87996f6aacc3c1a3a13972859215f4981
Veronica Paladino <veronica.paladino@mercadolibre.com>
2022-02-17 22:30:23 -0300

    merge con master

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
-const express = require('express')
+const express = require("express");
 const mysql = require("mysql2");
-var pool =mysql.createPool(
-  { connectionLimit   :   100,
-      host              :   '127.0.0.1',
-       user              :   'admin',
-       password          :   'admin',
-       database          :   'proyecto',
-       debug             :   false
-     });
 
-     
-const http = require('http')
-const path = require('path')
-const socketIO = require('socket.io')
+var pool = mysql.createPool({
+  connectionLimit: 100,
+  host: "127.0.0.1",
+  user: "admin",
+  password: "admin",
+  database: "proyecto",
+  debug: false,
+});
 
-const app = express()
-const server = http.Server(app)
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.Server(app);
 
 const io = socketIO(server, {
   pingTimeout: 60000,
-})
+});
 
 var puertoServidor = 8081;
 //var puertoServidor = 3306; // PUERTO VERO
 
-app.set('port', puertoServidor)
-app.use('/client', express.static(__dirname + '/client'))
+app.set("port", puertoServidor);
+app.use("/client", express.static(__dirname + "/client"));
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/client/index.html')) // Ruta para lanzar el index
-})
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "/client/index.html")); // Ruta para lanzar el index
+});
 
 // Inicializamos el server
 server.listen(puertoServidor, () => {
-  console.log('Starting server on port '+ puertoServidor)
-})
+  console.log("Starting server on port " + puertoServidor);
+});
 
-const players = {}
+const players = {};
 
 // Conexión de jugador
-io.on('connection', (socket) => {
-  
+io.on("connection", (socket) => {
   /*console.log('player [' + socket.id + '] connected')*/
 
   players[socket.id] = {
@@ -48,47 +47,38 @@ io.on('connection', (socket) => {
     x: 30,
     y: 30,
     playerId: socket.id,
-    color: getRandomColor()
-  }
+    color: getRandomColor(),
+  };
 
-  socket.emit('currentPlayers', players)
-  socket.broadcast.emit('newPlayer', players[socket.id])
- 
-  
-  socket.on('disconnect', () => {
-    console.log('player [' + socket.id + '] disconnected')
-    delete players[socket.id]
-    io.emit('playerDisconnected', socket.id)
-  })
+  socket.emit("currentPlayers", players);
+  socket.broadcast.emit("newPlayer", players[socket.id]);
 
-  
-  socket.on('playerMovement', (movementData) => {
-    players[socket.id].x = movementData.x
-    players[socket.id].y = movementData.y
-    players[socket.id].rotation = movementData.rotation
+  socket.on("disconnect", () => {
+    console.log("player [" + socket.id + "] disconnected");
+    delete players[socket.id];
+    io.emit("playerDisconnected", socket.id);
+  });
 
-    socket.broadcast.emit('playerMoved', players[socket.id])
-  })
+  socket.on("playerMovement", (movementData) => {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
 
-})
-
-
+    socket.broadcast.emit("playerMoved", players[socket.id]);
+  });
+});
 
 // deveulve un color Random
 function getRandomColor() {
-  return '0x' + Math.floor(Math.random() * 16777215).toString(16)
+  return "0x" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
+//BASE DE DATOS - CONEXION
+//conectarDB();
 
-
- //BASE DE DATOS - CONEXION
- //conectarDB();
-  
-
-  
 // function conectarDB()
 // {
- 
+
 //   // Conectando a mysql
 //   const conexion    =    mysql.createConnection({
 //     connectionLimit   :   100,
@@ -105,6 +95,6 @@ function getRandomColor() {
 //     if (err) throw err;
 //     console.log('Usuario: ', rows[0] );
 //   });
-  
+
 //   conexion.end();
 // }
