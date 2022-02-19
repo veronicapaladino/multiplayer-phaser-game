de75557036ef4dfcbd57203acdd6acc560c54e19
dxp0610 <73371657+dxp0610@users.noreply.github.com>
2022-02-19 19:33:15 -0300

    Add files via upload

--- client/Scenes/MenuScene.js
+++ client/Scenes/MenuScene.js
@@ -1,29 +1,49 @@
-//Escena inicial del juego...
-class MenuScene extends Phaser.Scene{
 
-    constructor () {
-        super({key: "MenuScene"});
-    }
+import BaseScene from './BaseScene';
 
-    create(){
+class MenuScene extends BaseScene {
 
-       
-        //this.add.sprite(0, 100, 'mouse');
-    
-        this.input.mouse.capture = true;
+  constructor(config) {
+    super('MenuScene', config);
 
-        const empezarPartida = this.add.text(100, 100, 'Empezar Partida', { fill: '#0f0' });
-        empezarPartida.setInteractive();
-    
-        empezarPartida.on('pointerdown', () => { 
-            console.log('cambiar de escena a: GameScene'); 
-            this.scene.start("GameScene");
-        });
-    }
+    this.menu = [
+      {scene: 'PlayScene', text: 'Play'},
+      {scene: 'ScoreScene', text: 'Score'},
+      {scene: null, text: 'Exit'},
+    ]
+  }
 
-   
+  create() {
+    super.create();
 
+    this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+  }
+
+  setupMenuEvents(menuItem) {
+    const textGO = menuItem.textGO;
+    textGO.setInteractive();
+
+    textGO.on('pointerover', () => {
+      textGO.setStyle({fill: '#ff0'});
+    })
+
+    textGO.on('pointerout', () => {
+      textGO.setStyle({fill: '#fff'});
+    })
+
+    textGO.on('pointerup', () => {
+      menuItem.scene && this.scene.start(menuItem.scene);
+
+      if (menuItem.text === 'Exit') {
+        this.game.destroy(true);
+      }
+    })
+  }
 }
 
-
 export default MenuScene;
+
+
+
+// Create ScoreScene
+// Display Best score, you can get from the local storage
