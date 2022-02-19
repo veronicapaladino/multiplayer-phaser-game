bfd4f91878ba14312cea4dd464028599e23ad6f1
dxp0610 <73371657+dxp0610@users.noreply.github.com>
2022-02-19 20:23:10 -0300

    Update and rename client/Scenes/PreloadScene.js to cliente/Escenas/PreloadScene.js

--- cliente/Escenas/PreloadScene.js
+++ cliente/Escenas/PreloadScene.js
@@ -7,11 +7,11 @@ class PreloadScene extends Phaser.Scene {
     super('PreloadScene');
   }
 
-  preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.spritesheet('bird', 'assets/birdSprite.png', {
-      frameWidth: 16, frameHeight: 16
+  preload()
+    this.load.image('water', 'assets/water.png');
+    this.load.spritesheet('ship', 'assets/shipSprite.png', {      frameWidth: 16, frameHeight:                                                      
     });
+
     this.load.image('pipe', 'assets/pipe.png');
     this.load.image('pause', 'assets/pause.png');
     this.load.image('back', 'assets/back.png');
